0277b62fda13042ffbe9ad00413f3a45f169d5ea
buterrr <butergut@gmail.com>
2021-05-04 09:42:49 +0300

    second commit

---
+++ classy_critter.py
@@ -0,0 +1,25 @@
+# Демонстрирует атрибуты класса и статические методы.
+
+class Critter(object):
+    total = 0
+
+    @staticmethod
+    def status():
+        print("\nВсего зверюшек сейчас", Critter.total)
+
+    def __init__(self, name):
+        print("Появилась на свет новая зверюшка!")
+        self.name = name
+        Critter.total += 1
+
+
+print("Нахожу значение атрибута класса Critter.total:", end=" ")
+print(Critter.total)
+Critter.status()
+print("\nСоздаю зверюшек.")
+crit1 = Critter("зверюшка 1")
+crit2 = Critter("зверюшка 2")
+crit3 = Critter("зверюшка 3")
+Critter.status()
+print("\nОбращаюсь к атрибуту класса через объект:", end=" ")
+print(crit1.total)
---
+++ constructor_critter.py
@@ -0,0 +1,13 @@
+class Critter(object):
+    def __init__(self):
+        print("Появилась на свет новая зверюшка!")
+
+    def talk(self):
+        print("\nПривет. Я зверюшка - экземпляр класса Critter.")
+
+
+crit1 = Critter()
+crit2 = Critter()
+crit1.talk()
+crit2.talk()
+input("\n\nНажмите Enter, чтобы выйти.")
